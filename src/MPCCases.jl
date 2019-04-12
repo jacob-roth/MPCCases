3538ee8952dc3a29cb2ce7a9091b8205aff8c091
Jake Roth <jmroth@uchicago.edu>
2019-04-12 12:50:23 -0500

    sparsearrays dep

--- src/MPCCases.jl
+++ src/MPCCases.jl
@@ -1,5 +1,6 @@
 module MPCCases
 using DelimitedFiles, StructArrays
+using SparseArrays
 ## based on: https://github.com/StructJuMP/StructJuMP.jl/tree/master/examples/PowerGrid
 
 include("load.jl")
