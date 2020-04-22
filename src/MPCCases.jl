fa51503a5790716b9b3127334a1ee1c309f7bbf6
Albert Lam <albert233@hotmail.com>
2020-04-22 15:49:39 -0500

    Refactored and added adjust.jl

--- src/MPCCases.jl
+++ src/MPCCases.jl
@@ -2,7 +2,6 @@ module MPCCases
 using DelimitedFiles, StructArrays
 using SparseArrays
 using Random
-using Statistics
 ## based on: https://github.com/StructJuMP/StructJuMP.jl/tree/master/examples/PowerGrid
 
 include("load.jl")
@@ -13,6 +12,8 @@ export mapGenersToBuses
 export computeAdmitances, computeAdmittances, computeAdmittanceMatrix
 export OPFData
 export CaseData
-export adj_params, complete_file_path, get_y_idx, generate_vals, reshape_vals, adj_vals_in_arr, cp_remaining_files, get_num_branches, get_mean_rateA_arr, write_agg_branch_file
+
+include("adjust.jl")
+export adj_params, complete_file_path, adj_params_and_write, cp_remaining_files
 
 end # module
