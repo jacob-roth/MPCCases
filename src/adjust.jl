fce7ce810bf5bcf8e8646a81c57e9a4ccf357b98
Albert Lam <albert233@hotmail.com>
2020-04-24 15:30:58 -0500

    Added a method to cp_remaining_files

--- src/adjust.jl
+++ src/adjust.jl
@@ -341,3 +341,12 @@ function cp_remaining_files(src_path::String, dst_path::String, file_name::Strin
         end
     end
 end
+
+function cp_remaining_files(src_path::Union{String, NTuple{N, String}}, dst_path::Union{String, NTuple{N, String}}, file_name::Union{String, NTuple{N, String}}) where {N}
+    src_path = isa(src_path, Union{String, Tuple{String}}) ? match_length(convert(String, src_path), N) : src_path
+    dst_path = isa(src_path, Union{String, Tuple{String}}) ? match_length(convert(String, dst_path), N) : dst_path
+    file_name = isa(file_name, Union{String, Tuple{String}}) ? match_length(convert(String, file_name), N) : file_name
+    for idx in 1:N
+        cp_remaining_files(src_path[idx], dst_path[idx], file_name[idx])
+    end
+end
