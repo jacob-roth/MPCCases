0dfa83b1cc458e4dd865c805af84b298261306c7
Albert Lam <albert233@hotmail.com>
2020-06-25 13:33:40 -0500

    Moved complete_base_files to util.jl

--- src/MPCCases.jl
+++ src/MPCCases.jl
@@ -5,7 +5,7 @@ using Random
 ## based on: https://github.com/StructJuMP/StructJuMP.jl/tree/master/examples/PowerGrid
 
 include("load.jl")
-export load_case, complete_base_files
+export load_case
 export mapLinesToBuses
 export mapBusIdToIdx, mapIdxToBusId
 export mapGenersToBuses
@@ -28,7 +28,8 @@ include("map_id.jl")
 export apply_mapping_to_file
 
 include("util.jl")
-export complete_file_path, get_y_idx, get_write_cols_idx, fill_write_file_path, match_length, cp_remaining_files, get_phys
+export complete_file_path, complete_base_files
+export get_y_idx, get_write_cols_idx, fill_write_file_path, match_length, cp_remaining_files, get_phys
 export get_Bs_psd_adjustments, update_case!
 
 end # module
