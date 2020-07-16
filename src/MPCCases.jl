672877d0d6305395a427650b8021c59f48dbb6dc
Albert Lam <albert233@hotmail.com>
2020-07-16 14:09:25 -0500

    Added agg_rank.jl

--- src/MPCCases.jl
+++ src/MPCCases.jl
@@ -27,13 +27,16 @@ export get_paste_vals, get_shed_bus_idx
 include("balance_slack.jl")
 export get_perturbed_xbar_arr, balance_slack, write_perturbed_Pg_file, get_true_scale_fac
 
+include("agg_rank.jl")
+export get_idx_arr, get_rank_arr, get_agg_rank_arr, get_agg_rank_perm
+
 include("map_id.jl")
 export apply_mapping_to_file
 
 include("util.jl")
 export complete_file_path, complete_base_files
 export get_y_idx, get_write_cols_idx, fill_write_file_path, match_length, cp_remaining_files
-export get_path_dict, get_case_file, add_gaussian_noise, undo_neg_vals, get_xbar_dict, get_xbar_arr
+export get_path_dict, get_case_arr, add_gaussian_noise, undo_neg_vals, get_xbar_dict, get_xbar_arr
 export get_phys
 export get_Bs_psd_adjustments, update_case!
 
---
+++ src/agg_rank.jl
@@ -0,0 +1,31 @@
+function get_idx_arr(idx...)
+    lengths = map(length, idx)
+    @assert all(x -> x == first(lengths), lengths) == true
+    idx_arr = Array{Int}(undef, length(idx), first(lengths))
+    for i in eachindex(idx)
+        idx_arr[i, :] = idx[i]
+    end
+    return idx_arr
+end
+
+function get_rank_arr(idx...)
+    idx_arr = get_idx_arr(idx)
+    rank_arr = similar(idx_arr)
+    for i in 1:size(idx_arr, 1)
+        for j in 1:size(idx_arr, 2)
+            rank_arr[i,j] = findfirst(isequal(j), idx_arr[i, :])
+        end
+    end
+    return rank_arr
+end
+
+function get_agg_rank_arr(idx...)
+    rank_arr = get_rank_arr(idx)
+    agg_rank_arr = sum(rank_arr, dims=1) ./ size(rank_arr, 1)
+    return agg_rank_arr
+end
+
+function get_agg_rank_perm(idx...)
+    agg_rank_arr = get_agg_rank_arr(idx)
+    return sortperm(vec(agg_rank_arr))
+end
